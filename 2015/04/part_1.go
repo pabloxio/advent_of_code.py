07300a41b4079f5f7e7df292aea937b9f9cfa169
Juan Pablo Jaramillo Pineda <juan@pablox.io>
2020-12-31 17:02:35 -0600

    [go] 2015 - day 4 - part 1

---
+++ 2015/04/part_1.go
@@ -0,0 +1,41 @@
+// Usage: From root folder
+// $ go run 2015/04/part_1.go 2015/04/input
+
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"fmt"
+	"os"
+)
+
+func miningAdventCoin(secret string) int {
+	number := 0
+	for {
+		data := []byte(secret + fmt.Sprintf("%d", number))
+		md5Sum := md5.Sum(data)
+		value := fmt.Sprintf("%x", md5Sum)[0:5]
+		if value == "00000" {
+			break
+		}
+		number++
+	}
+
+	return number
+}
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		secret := scanner.Text()
+		fmt.Printf("secret(%v) = %d\n", secret, miningAdventCoin(secret))
+	}
+}
