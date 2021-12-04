ecbff6fdfa126fa337be12e1dd877dd64ae06d88
Pablo Jaramillo <juan@pablox.io>
2021-12-04 11:53:39 -0600

    2021 - day 04

---
+++ 2021/04/part_2.py
@@ -0,0 +1,61 @@
+#  Usage: From root folder
+#  $ python 2021/04/part_1.py 2021/04/input
+
+import sys
+from typing import List
+
+
+def final_score(
+    board: List[List[int]],
+    numbers: List[int]
+) -> int:
+    board_numbers = []
+    for line in board:
+        board_numbers.extend(line)
+
+    return sum(set(board_numbers).difference(numbers)) * numbers[-1]
+
+
+def is_board_winner(
+    board: List[List[int]],
+    numbers: List[int],
+) -> bool:
+    # Horizontal
+    for line in board:
+        if set(line).issubset(numbers):
+            return True
+
+    # Vertical
+    for i in range(len(board)):
+        vertical = [line[i] for line in board]
+        if set(vertical).issubset(numbers):
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as file:
+        bingo = [line.strip() for line in file.readlines()]
+
+    numbers = [int(number) for number in bingo[0].split(",")]
+    boards = []
+    board = []
+    for line in bingo[2:]:
+        if line == "":
+            boards.append(board)
+            board = []
+            continue
+        board.append([int(n) for n in line.split()])
+
+    for i in range(len(numbers)):
+        if len(boards) > 1:
+            boards = [
+                b for b in boards
+                if not is_board_winner(b, numbers[:i])
+            ]
+            continue
+
+        if is_board_winner(boards[0], numbers[:i]):
+            print(final_score(boards[0], numbers[:i]))
+            break
